5e8143c31bb17977454f2bd96b8aca9fbfab02f4
effervescent000 <kislio01@gmail.com>
2023-04-08 15:03:52 -0400

    persist profiles

--- tests/profiles.spec.ts
+++ tests/profiles.spec.ts
@@ -3,10 +3,17 @@ import { expect, test } from '@playwright/test';
 test.describe('tests re: profile functionality', () => {
 	test.beforeEach(async ({ page }) => {
 		await page.goto('/');
+		await page.evaluate(() => window.localStorage.clear());
 	});
 
 	test('can add a new profile', async ({ page }) => {
 		await page.getByTestId('new-profile-btn').click();
+		await expect(page.getByTestId('profile-0')).toBeVisible();
+	});
+
+	test('persists a profile through page loads', async ({ page }) => {
+		await page.getByTestId('new-profile-btn').click();
+		await page.reload();
 		await expect(page.getByTestId('profile-0')).toBeVisible();
 	});
 });
