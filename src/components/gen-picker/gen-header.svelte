573146057ec9ff8f5d8e29bb2216ed6b531a1d09
effervescent000 <kislio01@gmail.com>
2023-04-08 12:27:55 -0400

    implement gen picker

---
+++ src/components/gen-picker/gen-header.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let version: string;
+	export let selectedVersion: number | string;
+	export let versionKey: number | string;
+	export let setVersion: (value: string | number) => void;
+</script>
+
+<button on:click={() => setVersion(versionKey)}>{version}</button>
