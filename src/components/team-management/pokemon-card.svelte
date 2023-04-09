2e0e4e841f16f5f3bf986ff914adb93c42bbc7ca
effervescent000 <kislio01@gmail.com>
2023-04-09 14:25:50 -0400

    minimal pokemon card

---
+++ src/components/team-management/pokemon-card.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { IPokemonFull, IPokemonSkeleton } from '../../typing/interfaces';
+
+	import { properCase } from '../../utils/generic-utils';
+
+	// PROPS
+
+	export let pokemon: IPokemonSkeleton;
+
+	// STATE
+
+	// let fullPokemon: IPokemonFull;
+</script>
+
+<div class="flex" data-testid={`poke-card-${pokemon.name}`}>
+	<div>
+		<span>
+			{properCase(pokemon.name)}
+		</span>
+		<div>
+			<!-- sprites and scoring info goes here -->
+		</div>
+	</div>
+	<div>
+		<!-- move selection goes here -->
+	</div>
+</div>
