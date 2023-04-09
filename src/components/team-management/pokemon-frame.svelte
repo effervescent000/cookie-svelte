803f88a7420a01735154d715cab412d9ed4ecdee
effervescent000 <kislio01@gmail.com>
2023-04-09 14:07:15 -0400

    create and render frames

---
+++ src/components/team-management/pokemon-frame.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	// import type { IPokemonSkeleton } from '../../typing/interfaces';
+	import { properCase } from '../../utils/generic-utils';
+
+	// PROPS
+
+	export let location: string;
+	// export let pokemon: IPokemonSkeleton[]
+</script>
+
+<div data-testid={`frame-${location}`}>
+	<span>{properCase(location)}</span>
+	<div class="grid min-h-40 min-w-max grid-cols-2 gap-2 border border-light-blue">
+		Nothing here yet.
+	</div>
+</div>
---
+++ src/routes/team/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import PokemonFrame from '../../components/team-management/pokemon-frame.svelte';
+</script>
+
+<PokemonFrame location="team" />
+<PokemonFrame location="bench" />
