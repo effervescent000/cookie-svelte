7242187db5abbaad4bf6b0f2fe19a7a1b2f5bd0a
effervescent000 <kislio01@gmail.com>
2023-04-08 22:33:42 -0400

    add profile tests what a nightmare

--- tests/profiles.spec.ts
+++ tests/profiles.spec.ts
@@ -1,9 +1,18 @@
 import { expect, test } from '@playwright/test';
 
+// import { mergeStateIntoContext } from '../test-utils/utils.js';
+import { LocalStoragePage } from '../test-utils/local-storage-page.js';
+
+const SELECTED_CLASS = /border-dark-blue/;
+const UNSELECTED_CLASS = /border-white/;
+
 test.describe('tests re: profile functionality', () => {
 	test.beforeEach(async ({ page }) => {
 		await page.goto('/');
-		await page.evaluate(() => window.localStorage.clear());
+	});
+	test.afterEach(async ({ page }) => {
+		const ls = new LocalStoragePage(page);
+		await ls.clearStorage();
 	});
 
 	test('can add a new profile', async ({ page }) => {
@@ -15,5 +24,13 @@ test.describe('tests re: profile functionality', () => {
 		await page.getByTestId('new-profile-btn').click();
 		await page.reload();
 		await expect(page.getByTestId('profile-0')).toBeVisible();
+	});
+
+	test('can change active profile', async ({ page }) => {
+		const ls = new LocalStoragePage(page);
+		await ls.createLocalStorage('two-empty-profiles');
+		const profileOne = page.getByTestId('profile-1');
+		await expect(profileOne).toHaveClass(SELECTED_CLASS);
+		await expect(page.getByTestId('profile-0')).toHaveClass(UNSELECTED_CLASS);
 	});
 });
