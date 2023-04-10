1e293df32d21289201a2db115328e06139f560a6
effervescent000 <kislio01@gmail.com>
2023-04-10 17:23:58 -0400

    STOP YELLING AT ME ESLINT

--- src/typing/type-guards.ts
+++ src/typing/type-guards.ts
@@ -1,7 +1,9 @@
 import type { IMoveFull, IPokemonFull, IResourceListItem } from './interfaces';
 
-export const isFullPokemon = (poke: IPokemonFull | IResourceListItem): poke is IPokemonFull =>
-	!!(poke as IPokemonFull).moves;
+export const isFullPokemon = (
+	poke: IPokemonFull | IResourceListItem
+): poke is IPokemonFull => !!(poke as IPokemonFull).moves;
 
-export const isFullMove = (move: IMoveFull | IResourceListItem): move is IMoveFull =>
-	!!(move as IMoveFull).generation;
+export const isFullMove = (
+	move: IMoveFull | IResourceListItem
+): move is IMoveFull => !!(move as IMoveFull).generation;
