a7c7f1fda07b530ebfa232c843cdf3d315f6b8ca
effervescent000 <kislio01@gmail.com>
2023-04-10 10:19:55 -0400

    expand components a bit

--- src/components/common/select.svelte
+++ src/components/common/select.svelte
@@ -2,10 +2,21 @@
 	// PROPS
 	export let value: string;
 	export let options: { value: string; name: string }[];
+	export let label: string = '';
+	export let testid: string = '';
 </script>
 
-<select bind:value>
-	{#each options as option}
-		<option value={option.value}>{option.name}</option>
-	{/each}
-</select>
+<div>
+	{#if label}
+		<span>{label}</span>
+	{/if}
+	<select
+		class="m-1 w-44 rounded-md border border-blue bg-gray p-1 text-sm"
+		data-testid={testid}
+		bind:value
+	>
+		{#each options as option}
+			<option value={option.value}>{option.name}</option>
+		{/each}
+	</select>
+</div>
--- src/components/common/text-input.svelte
+++ src/components/common/text-input.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
 	// PROPS
 	export let value: string;
+	export let label: string = '';
+	export let testid: string = '';
 </script>
 
-<input bind:value />
+<div>
+	{#if label}
+		<span>{label}</span>
+	{/if}
+	<input
+		class={'m-1 w-48 rounded-md border border-light-blue bg-gray p-1'}
+		data-testid={testid}
+		bind:value
+	/>
+</div>
